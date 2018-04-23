4bd73e2efdcce829a59fd8f56152b394071e2c32
turboMan <SXXT2015@outlook.com>
2018-04-23 17:48:46 +0800

    CH1_4,Algorithm Analysis ,TwoSumBasic&Fast,ThreeSumBasic&Fast,FlashXT,2018.4.23,Monday

--- src/CH1/CH1_1/BinarySearch.java
+++ src/CH1/CH1_1/BinarySearch.java
@@ -23,13 +23,13 @@ public class BinarySearch {
 
         int low = 0;
         int high = a.length - 1;
-
+        int mid = low + ( high - low ) / 2;
         while(low <= high){
-            int mid = low + ( high - low ) / 2;
+
             if( key < a[mid] )           high = mid - 1;
             else if (key > a[mid])        low = mid + 1;
             else                          return mid;
-
+            mid = low + ( high - low ) / 2;
         }
         return -1;
     }
