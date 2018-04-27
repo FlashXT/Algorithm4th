f03b232041062a93de52567a2d615f20f36ab786
turboMan <SXXT2015@outlook.com>
2018-04-27 14:46:37 +0800

    TwoSumBase complete ,Author：FlashXT; Date :2018.4.27,Friday;

---
+++ src/CH1/CH1_4/SumProblem/TwoSumBase.java
@@ -0,0 +1,28 @@
+package CH1.CH1_4.SumProblem;
+
+import CH1.CH1_4.Stopwatch;
+import edu.princeton.cs.algs4.In;
+
+public class TwoSumBase {
+    public static void main(String [] args){
+        int count = 1;int cnt = 0;
+        System.out.println("Scale\tCount\tTime");
+        System.out.println("----------------------");
+        while(count <= 32){
+            int [] data = In.readInts("src\\CH1\\CH1_4\\"+count+"Kints.txt");
+
+            Stopwatch timer = new Stopwatch();
+            cnt = count(data);
+            System.out.printf("%-2dK\t\t%3d\t\t%.3fs\n",count,cnt,timer.elapsedTime());
+            count*=2;
+        }
+
+    }
+    public static int count(int [] data){
+        int cnt = 0;
+        for(int i = 0; i < data.length ; i++)
+            for(int j = i+1; j < data.length ; j++ )
+                if(data[i] + data[j] == 0) cnt++;
+        return cnt;
+    }
+}
