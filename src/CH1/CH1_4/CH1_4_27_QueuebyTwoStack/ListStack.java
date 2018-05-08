8e4607bdc0c2544d67b65b515f625b6731937972
T430 <turboman2017@qq.com>
2018-05-08 23:17:03 +0800

    complete queue by two stack;Author:turboMan,Date:2018.5.8,Tuesday.

---
+++ src/CH1/CH1_4/CH1_4_27_QueuebyTwoStack/ListStack.java
@@ -0,0 +1,62 @@
+/*Algorithm4th,CH1.3,利用链表实现Stack
+ *Author：FlashXT;
+ * Date:2018.4.6,Friday;
+ * */
+package CH1.CH1_4.CH1_4_27_QueuebyTwoStack;
+
+import java.util.Iterator;
+
+public class ListStack<Item> implements  Iterable<Item> {
+    private Node first;
+    private int N;
+    //私有嵌套类的一个特点是只有包含它的类能够直接访问它的实例变量，
+    //因此无需将它的实例变量声明为public 或者private;
+    private class Node{
+        Item item;
+        Node next;
+
+    }
+//    public ListStack(){
+//        Node node = new Node();
+//        first = node;
+//    }
+    public boolean isEmpty(){return first == null;}
+    public int size(){return N;}
+    //入栈
+    public void push(Item item){
+//        Node node = new Node();
+//        node.item = item;
+//        node.next = first.next;
+//        first.next = node;
+
+        Node node = first;
+        first = new Node();
+        first.item = item;
+        first.next = node;
+        N++;
+    }
+    //出栈
+    public Item pop(){
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+    //获取栈顶元素
+    public Item peek(){
+        if(isEmpty()) throw new RuntimeException("Stack underflow");
+        return first.item;
+    }
+    //迭代
+    public Iterator<Item> iterator(){ return new ListIterator();}
+    private class ListIterator implements Iterator<Item>{
+        private Node current = first;
+        public boolean hasNext(){   return current != null;}
+        public Item next(){
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+        public void remove(){}
+    }
+}
---
+++ src/CH1/CH1_4/CH1_4_27_QueuebyTwoStack/QueueByStack.java
@@ -0,0 +1,102 @@
+/***********************************************************************
+ * 题目：用两个Stack实现Queue；
+ * 算法分析：将所有元素压入栈再弹出，元素的顺序就颠倒了，再次重复这个工程，元素的顺序
+ *         就会复原。
+ *
+ * Author:FlashXT;Date:2018.5.8,Tuesday;
+ * CopyRight © 2018-2020,FlashXT & turboMan . All Right Reserved.
+ ***********************************************************************/
+
+
+package CH1.CH1_4.CH1_4_27_QueuebyTwoStack;
+
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
+public class QueueByStack<Item> implements Iterable<Item>{
+
+   private   ListStack< Item> ls0;
+   private   ListStack<Item> ls1;
+    public QueueByStack(){
+        ls0 = new ListStack<Item>();
+        ls1 = new ListStack<Item>();
+    }
+
+    //入队
+    public  void  enqueue(Item item){
+        ls0.push(item);
+    }
+    //出队
+    public  Item  enqueue(){
+        Item temp;
+        while(!ls0.isEmpty()){
+            temp = ls0.pop();
+            ls1.push(temp);
+        }
+        Item temp1= ls1.pop();
+        while(!ls1.isEmpty()){
+            temp = ls1.pop();
+            ls0.push(temp);
+        }
+        return temp1;
+    }
+
+    public boolean isEmpty(){
+        return ls0.isEmpty();
+    }
+    public int size(){
+        return ls0.size();
+    }
+    public Item peek(){
+        Item temp;
+        while(!ls0.isEmpty()){
+            temp = ls0.pop();
+            ls1.push(temp);
+        }
+        Item temp1= ls1.peek();
+        while(!ls1.isEmpty()){
+            temp = ls1.pop();
+            ls0.push(temp);
+        }
+        return temp1;
+    }
+
+    public Iterator<Item> iterator(){
+        Item temp;
+        while(!ls0.isEmpty()){
+            temp = ls0.pop();
+            ls1.push(temp);
+        }
+        return  ls1.iterator();
+    }
+
+    public void Traveral(){
+        Iterator<Item> itor = this.iterator();
+        while(itor.hasNext()){
+            Item item = itor.next();
+            ls0.push(item);
+            StdOut.print(item+" ");
+        }
+    }
+
+    public static void main(String [] args){
+
+        QueueByStack<String > queue = new QueueByStack<String>();
+        queue.enqueue("1");
+        queue.enqueue("2");
+        queue.enqueue("3");
+        queue.enqueue("4");
+        StdOut.println(queue.size());
+        queue.Traveral();
+        StdOut.println("\n"+queue.size());
+        StdOut.println( queue.peek());
+
+
+    }
+
+
+
+
+}
